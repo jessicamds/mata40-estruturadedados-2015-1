76d6b4922579dfc866023028d441a67e50766180
jessicamds <jessica.mcds@gmail.com>
2015-03-29 19:47:42 -0300

    Delete L1-Q14.c

--- atividade 01/L1-Q14.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main (){
-	float s,a; // s = salario, a = percentual de aumento.
-	printf ("Digite o salario base:\n");
-	scanf ("%f", &s);
-	a = 0.25*s;
-	printf ("novo salario: %f\n", s+a);
-	return 0;
-}
